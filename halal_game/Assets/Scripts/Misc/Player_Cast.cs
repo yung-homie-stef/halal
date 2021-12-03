24224aca9d78fca5d043dff4ead7d82bab9d258a
Stef Pinto <stefanopinto1@gmail.com>
2021-12-02 19:35:24 -0500

    Player interaction script

---
+++ halal_game/Assets/Scripts/Misc/Player_Cast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Cast : MonoBehaviour
+{
+    public Camera playerCamera;
+    public RaycastHit playerRaycastHit;
+
+    void Update()
+    {
+        Debug.DrawRay(playerCamera.transform.position, transform.forward, Color.green);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Interact();
+        }
+    }
+
+    void Interact()
+    {
+        Ray _ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        GameObject objectToInteractWith;
+
+        if (Physics.Raycast(_ray, out playerRaycastHit, 1))
+        {
+            if (playerRaycastHit.transform.tag.Equals("Interactable"))
+            {
+                objectToInteractWith = playerRaycastHit.transform.gameObject;
+
+                //objectToInteractWith.GetComponent<KeyInteraction>().Activate();
+            }
+        }
+    }
+
+}
